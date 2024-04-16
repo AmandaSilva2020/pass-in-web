0dc2814531b1eac319aa16c52f813460c7dd5a2d
Amanda Silva <amanda328@hotmail.com>
2024-04-16 22:37:48 +0100

    remove outline from search input

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -45,7 +45,7 @@ export function AttendeeList(){
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className="size-4 text-emerald-300" />
-                    <input onChange={event => onSearchInputChange(event)} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input onChange={event => onSearchInputChange(event)} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Buscar participante..." />
                 </div>
             </div>
 
